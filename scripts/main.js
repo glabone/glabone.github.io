059f2163555499852a2440370f39e30991672365
glabone <50001032+glabone@users.noreply.github.com>
2020-05-22 14:17:26 -0600

    .

--- scripts/main.js
+++ scripts/main.js
@@ -48,12 +48,16 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span1 = document.getElementsByClassName("close1")[0];
 // When the user clicks the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+  modal.style.display = "none";
+}
+span1.onclick = function() {
   modal.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
