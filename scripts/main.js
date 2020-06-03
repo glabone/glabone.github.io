20d035a64fe012a609f24a8a23e6fe767301328a
glabone <50001032+glabone@users.noreply.github.com>
2020-06-03 00:54:17 -0600

    .

--- scripts/main.js
+++ scripts/main.js
@@ -44,25 +44,76 @@ $('a.smooth-scroll')
 
 // Get the modal
 var modal = document.getElementById("myModal");
+var modal1 = document.getElementById("myModal1");
+var modal_img1 = document.getElementById("modal_img1");
+var modal_img2 = document.getElementById("modal_img2");
+var modal_img3 = document.getElementById("modal_img3");
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btn1 = document.getElementById("myBtn1");
+var btn_img1 = document.getElementById("btn_img1");
+var btn_img2 = document.getElementById("btn_img2");
+var btn_img3 = document.getElementById("btn_img3");
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-var span1 = document.getElementsByClassName("close1")[0];
+//var span = document.getElementsByClassName("close")[0];
+//var span1 = document.getElementsByClassName("close1")[0];
+var span = document.getElementById("close");
+var spanA = document.getElementById("closeA");
+var span1 = document.getElementById("close1");
+var span1A = document.getElementById("close1A");
 // When the user clicks the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+}
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+btn_img1.onclick = function() {
+  modal_img1.style.display = "block";
+}
+btn_img2.onclick = function() {
+  modal_img2.style.display = "block";
+}
+btn_img3.onclick = function() {
+  modal_img3.style.display = "block";
 }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-span1.onclick = function() {
+spanA.onclick = function() {
   modal.style.display = "none";
+}
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+span1A.onclick = function() {
+  modal1.style.display = "none";
+}
+modal_img1.onclick = function() {
+  modal_img1.style.display = "none";
+}
+modal_img2.onclick = function() {
+  modal_img2.style.display = "none";
+}
+modal_img3.onclick = function() {
+  modal_img3.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target === modal) {
     modal.style.display = "none";
+  }
+  else if (event.target === modal1) {
+    modal1.style.display = "none";
+  }
+  else if (event.target === modal_img1) {
+    modal_img1.style.display = "none";
+  }
+  else if (event.target === modal_img2) {
+    modal_img2.style.display = "none";
+  }
+  else if (event.target === modal_img3) {
+    modal_img3.style.display = "none";
   }
 }
